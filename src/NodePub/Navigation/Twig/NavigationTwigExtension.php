6fe9a594cbbf0acbe5514f48948ff16f07b5a70e
Andrew Gruner <andrew.gruner@gmail.com>
2013-11-10 20:41:59 -0800

    Adding view templates.

--- src/NodePub/Navigation/Twig/NavigationTwigExtension.php
+++ src/NodePub/Navigation/Twig/NavigationTwigExtension.php
@@ -7,9 +7,8 @@ use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class NavigationTwigExtension extends \Twig_Extension
 {
-    private $sitemap;
-
-    protected $twigEnvironment;
+    protected $twigEnvironment,
+              $sitemap;
 
     public function __construct(Sitemap $sitemap)
     {
@@ -19,6 +18,8 @@ class NavigationTwigExtension extends \Twig_Extension
     public function initRuntime(\Twig_Environment $environment)
     {
         $this->twigEnvironment = $environment;
+        $loader = $environment->getLoader();
+        $loader->addSource(__DIR__.'../Resorces/views');
     }
 
     public function getName()
@@ -29,13 +30,23 @@ class NavigationTwigExtension extends \Twig_Extension
     public function getFunctions()
     {
         return array(
-            'breadcrumbs'        => new \Twig_Function_Method($this, 'breadcrumbs'),
+            'breadcrumbs'      => new \Twig_Function_Method($this, 'breadcrumbs'),
+            'breadcrumb_links' => new \Twig_Function_Method($this, 'breadcrumbLinks'),
+            'menu'             => new \Twig_Function_Method($this, 'menu'),
         );
     }
 
-    // returns rendered string
-    public function breadcrumbs()
+    public function menu()
     {
+        return $this->twigEnvironment->render('menu.twig', array());
+    }
+
+    // returns rendered string
+    public function breadcrumbs($separator)
+    {
+        return $this->twigEnvironment->render('breadcrumbs.twig', array(
+            'breadcrumb_items' => $this->sitemap->getActiveNodes()
+        ));
     }
 
     public function breadcrumbLinks()
